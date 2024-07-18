21b8620b541b3ba003b4f6cdbf34ea63f8451718
Aldenir Dos Santos <aldenirtakeshi@gmail.com>
2024-07-18 11:32:41 -0400

    commit01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  title = "Minha primeira aplição Angular";
+  states:any[] = []
+  cities:any[] = []
+  selectedState: string = ''
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.dataService.getStates().subscribe(
+      (response) => {
+        this.states = response;
+      },
+      (error) => {
+        console.error('Erro ao obter estados', error);
+      }
+    );
+  }
+
+  onStateChange(): void {
+    this.dataService.getCities(this.selectedState).subscribe(
+      (response) => {
+        this.cities = response;
+      },
+      (error) => {
+        console.error('Erro ao obter cidades', error);
+      }
+    );
+  }
+
 }
